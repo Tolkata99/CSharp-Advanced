d8e7b619d83dd4f6938febc859d5f2ab3cfb66dc
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-10 11:18:27 +0200

    I solved MethodStack from lab-SoftUni.

---
+++ MethodsAdv/MethodsAdv/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace MethodsAdv
+{
+    class Program
+    {
+        public static int Add(params int[]  nums)
+        {
+            return  nums.Sum();
+        }
+        public static int Add(int b,int a)
+        {
+            return b + a;
+        }
+        public static double Add(double b, double a)
+        {
+            return b * a;
+        }
+       
+        public static int Add(int b, int a, int c)
+        {
+
+            return b + a + c;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Add(a , b));
+        }
+    }
+}
