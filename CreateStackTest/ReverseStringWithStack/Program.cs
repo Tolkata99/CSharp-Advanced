b8126bea86a9a862725280c1753c45a488c2946b
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-10 11:17:23 +0200

    I solved ReverseStringWithStack from lab-SoftUni.

---
+++ CreateStackTest/ReverseStringWithStack/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseStringWithStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stackReverse = new Stack<char>();
+
+            string words = (Console.ReadLine());
+
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                stackReverse.Push(words[i]);
+            }
+
+           
+            while(stackReverse.Count > 0)
+            {
+                Console.Write(stackReverse.Pop());
+            }
+
+            
+        }
+    }
+}
