1f71086b216c19632550f9666b163db7cf3d8448
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-10 11:17:04 +0200

    StackTest

---
+++ CreateStackTest/CreateStackTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CreateStackTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            string book = Console.ReadLine();
+
+            while(book != "end")
+            {
+                stack.Push(book);
+                book = Console.ReadLine();
+
+            }
+
+            while(stack.Count > 0)
+            {
+                Console.WriteLine(stack.Peek());
+                Console.WriteLine(stack.Pop());
+            }
+            
+        }
+    }
+}
