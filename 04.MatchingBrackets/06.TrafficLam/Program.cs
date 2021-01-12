aa02de8ac6efc1ade21623630cae1ca1fde1a665
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-12 11:13:28 +0200

    I solved HotPotato from C#Advanced-Lab

---
+++ 04.MatchingBrackets/06.TrafficLam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.TrafficLam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenCount = int.Parse(Console.ReadLine());
+            string car = Console.ReadLine();
+            Queue<string> queue = new Queue<string>();
+            int count = 0;
+
+            while (car != "end")
+            {
+                
+
+                if(car == "green")
+                {
+                    for (int i = 0; i < greenCount; i++)
+                    {
+                        if (queue.Count == 0) break;
+                        count++;
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(car);
+                }
+
+                car = Console.ReadLine();
+            }
+            Console.WriteLine($"{count} cars passed the crossroads");
+        }
+    }
+}
