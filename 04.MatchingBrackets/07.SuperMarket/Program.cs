f32e59cb148376ea7a0ccfa7fa611a594ec0a722
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-12 11:17:00 +0200

    I solved Maching from C#Advanced-Lab

---
+++ 04.MatchingBrackets/07.SuperMarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.SuperMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = Console.ReadLine();
+            Queue<string> queue = new Queue<string>();
+
+            while (names != "End")
+            {
+                if(names == "Paid")
+                {
+                    while (queue.Count > 0) //greshka
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                       // queue.Dequeue();
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(names);
+                }
+
+
+
+                names = Console.ReadLine();
+            }
+            Console.WriteLine($"{queue.Count} people remaining");
+        }
+    }
+}
