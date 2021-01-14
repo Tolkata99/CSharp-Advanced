160a777fa4a1bf01a2dc48fcde84d42fe1ec4c37
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-14 12:19:44 +0200

    I solved Maching from Exercise(StackAndQueue)!

---
+++ 04.MatchingBrackets/PrintEvenNumbersWithQueue/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbersWithQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> queue = new Queue<int>(input);
+            int count = queue.Sum();
+            int sum = 0;
+            while (sum != count)
+            {
+                sum += queue.Peek();
+               // Console.WriteLine(queue.Dequeue());
+                if (queue.Peek() % 2 == 1)
+                {
+                    queue.Dequeue();
+                }
+                else
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+            }
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
