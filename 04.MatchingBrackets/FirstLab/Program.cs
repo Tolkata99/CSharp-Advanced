d256385938ba9d8b544f12bd6ce01a1bd3172e4c
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-14 12:20:09 +0200

    I solved FirstLab from Lab(StackAndQueue)!

---
+++ 04.MatchingBrackets/FirstLab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FirstLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expressions = (Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < expressions.Length; i++)
+            {
+                if(expressions[i] == '(')
+                {
+                    stack.Push(i);
+                }
+
+                if(expressions[i] == ')')
+                {
+                    int start = stack.Pop();
+                    Console.WriteLine(expressions.Substring(start, i - start + 1));
+                }
+
+                
+            }
+
+        }
+    }
+}
