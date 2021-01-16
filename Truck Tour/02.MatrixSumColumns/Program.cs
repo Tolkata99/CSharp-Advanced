9ea2bcdad014fb09c69d8a67f9b1899f5c2cc603
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-16 14:07:51 +0200

    I solved MatrixSumColums from SoftUni.

---
+++ Truck Tour/02.MatrixSumColumns/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.MatrixSumColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowAndCol = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = rowAndCol[0];
+            int cols = rowAndCol[1];
+            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] AddElements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = AddElements[col];
+                }
+            }
+
+            for (int coll = 0; coll < matrix.GetLength(1); coll++)
+            {
+                sum = 0;
+                for (int roww = 0; roww < matrix.GetLength(0); roww++)
+                {
+                    sum += matrix[roww, coll];
+                    
+                }
+                Console.WriteLine(sum);
+
+            }
+        }
+    }
+}
