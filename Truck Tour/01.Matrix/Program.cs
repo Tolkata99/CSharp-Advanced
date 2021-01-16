9e63c4acc169c3c9358716e180768ba39c55a8d1
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-16 14:07:30 +0200

    I solved Matrix from SoftUni.

---
+++ Truck Tour/01.Matrix/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] rolsAndColl = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = rolsAndColl[0];
+            int colls = rolsAndColl[1];
+            int sum = 0;
+
+
+            int[,] matrix = new int[rows, colls];
+
+            for (int rols = 0; rols < matrix.GetLength(0); rols++)
+            {
+                int[] addElements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rols, cols] = addElements[cols];
+
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int coll = 0; coll < matrix.GetLength(1); coll++)
+                {
+                    sum += matrix[row, coll];
+                }
+            }
+
+
+
+
+            Console.WriteLine(rows);
+            Console.WriteLine(colls);
+            Console.WriteLine(sum);
+        }
+
+    }
+}
