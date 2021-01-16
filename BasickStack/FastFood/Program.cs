ce950dd4c821aaf9b27f63a5196c13e51d3db232
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-16 14:02:02 +0200

    I solved FastFood from SoftUni.

---
+++ BasickStack/FastFood/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityFood = int.Parse(Console.ReadLine());
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(numbers);
+
+            if (queue.Count > 0)
+                Console.WriteLine(queue.Max());
+            while (queue.Count > 0)
+            {
+                int currentInt = queue.Peek();
+
+                if (quantityFood - currentInt >= 0)
+                {
+                    quantityFood -= currentInt;
+                    queue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+            }
+        }
+    }
+}
