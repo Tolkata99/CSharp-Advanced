85df0fce0f4564c66b3b13af7143ac96c9257c02
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-16 14:01:06 +0200

    I solved Fashion Boutique from SoftUni.

---
+++ BasickStack/Fashion Boutique/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dressBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int maxCapacity = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>(dressBox);
+            
+            int count = 0;
+            int sum = 0;
+            while (stack.Count > 0)
+            {
+                var current = stack.Peek();
+
+                if(sum + current > maxCapacity)
+                {
+                    count++;
+                    sum = 0;
+                }
+                else if(sum + current == maxCapacity)
+                {
+                    count++;
+                    sum = 0;
+                    stack.Pop();
+                }
+                else
+                {
+                    sum += stack.Pop();
+                }
+            }
+            if(sum > 0)
+            {
+                count++;
+            }
+            Console.WriteLine(count);
+
+        }
+    }
+}
