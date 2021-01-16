1175ce5ca0decfbdee59cb3c62f6778a261a3c00
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-16 13:59:47 +0200

    I solved Basic Queue Operations from SoftUni.

---
+++ BasickStack/Basic Queue Operations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var comand = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(numbers);
+
+
+            for (int i = 0; i < comand[1]; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if(queue.Contains(comand[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else if(queue.Count > 0 )
+            {
+                Console.WriteLine(queue.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
