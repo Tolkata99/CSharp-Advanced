2dc43f37a7dd3be9c3b182e0c17959fbd7b3d75a
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-22 16:26:24 +0200

    StreamDemo

---
+++ StreamDemo/StreamDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace StreamDemo
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader("../../../StreamDemo.txt"))
+            {
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    if(count % 2 == 1)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    Console.WriteLine();
+                    line = reader.ReadLine();
+                    count++;
+                }
+                
+            }
+        }
+    }
+}
