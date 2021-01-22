dcaa2cd9e4166db4d26b6895eef337737802eb13
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-22 16:26:46 +0200

    I solved LineNum

---
+++ StreamDemo/LineNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../TextFileFirst.txt").ToArray();
+
+            string[] end = new string[lines.Length];
+            for (int i = 0; i < lines.Length; i++)          
+            {
+                end[i] = $"{i} " + lines[i];
+               
+            }
+            
+            File.WriteAllLines("../../../TextFileSecond.txt", lines);
+            Console.WriteLine(File.ReadAllLines("../../../TextFileSecond.txt"));
+
+            
+
+
+            
+
+
+        }
+    }
+}
