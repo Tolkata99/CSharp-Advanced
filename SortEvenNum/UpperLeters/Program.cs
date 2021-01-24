7e568947b57384a1e50c7f5626da00d728bc15fd
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-24 07:40:49 +0200

    I solved UpperLetters

---
+++ SortEvenNum/UpperLeters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace UpperLeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            var words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(l => l[0] == l.ToUpper()[0]).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+
+        }
+    }
+}
