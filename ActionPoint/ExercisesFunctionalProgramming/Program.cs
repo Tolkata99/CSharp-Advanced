77fab869093b14a7645646a70247284efa4eab3d
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-25 19:09:04 +0200

    FunctionalPrograming

---
+++ ActionPoint/ExercisesFunctionalProgramming/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ExercisesFunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> printOntoName = (name) =>
+            {
+                Console.WriteLine(name);
+            };
+
+            Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(printOntoName);
+        }
+    }
+}
