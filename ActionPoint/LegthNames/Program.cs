eff332687c1c1e40baa97a10ee4318241adda73c
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-25 19:08:13 +0200

    I solved LengthNames

---
+++ ActionPoint/LegthNames/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LengthNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            string[] array = Console.ReadLine().Split().ToArray();
+
+            
+            Func<string[], string[]> filterSize = (length) =>
+             {
+                 for (int i = 0; i < array.Length; i++)
+                 {
+                     if (size < array[i].Length)
+                     {
+                         array[i] = "";
+                     }
+
+                 }
+
+                 return array;
+             };
+            
+            
+            foreach (var item in filterSize(array))
+            {
+                if(item != "")
+                Console.WriteLine(item + " ");
+            }
+        }
+    }
+}
