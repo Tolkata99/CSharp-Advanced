003176683d7c716dd1940fec14ae79630828f72c
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-26 16:38:44 +0200

    Car

---
+++ CarManufacturer/CarManufacturer/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double consumation = distance * this.FuelQuantity;
+            if (this.FuelConsumption - consumation <= 0)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+            else
+            {
+                this.FuelQuantity -= consumation;
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+
+
+    }
+}
---
+++ CarManufacturer/CarManufacturer/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "houp";
+            car.Model = "53u0";
+            car.Year = 7;
+            car.FuelConsumption = 200;
+            car.FuelQuantity = 200;
+
+
+            car.Drive(20);
+            car.Drive(10);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
