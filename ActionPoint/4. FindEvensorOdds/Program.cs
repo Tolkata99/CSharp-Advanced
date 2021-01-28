32556f3f98de3695d3b23f14a9cf6b5ec739f604
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-28 11:43:40 +0200

    Solved

---
+++ ActionPoint/4. FindEvensorOdds/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._FindEvensorOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bounds = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string comand = Console.ReadLine();
+
+
+            Predicate<int> predicate = comand == "odd" ?
+                new Predicate<int>((n) => n % 2 != 0) :
+                new Predicate<int>((n) => n % 2 == 0);
+
+            for (int i = bounds[0]; i < bounds[1]; i++)
+            {
+                if(predicate)
+            }
+
+        }
+    }
+}
