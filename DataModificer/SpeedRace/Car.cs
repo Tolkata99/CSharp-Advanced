4cc6b0e4765bd470e1f0342df4f8441a539df9e4
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-30 07:44:55 +0200

    I solved SpeedRace from Classes

---
+++ DataModificer/SpeedRace/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRace
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            TravelledDistance = 0;
+        }
+
+
+
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TravelledDistance { get; set; }
+
+
+
+
+        public void Distance(double fuelAmoth, double fuelOneKm,double distance)
+        {
+            double GetFuel = distance * fuelOneKm;
+
+            if(fuelAmoth >= GetFuel)
+            {
+                this.FuelAmount -= GetFuel;
+                this.TravelledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+
+        }
+
+       
+
+
+    }
+}
---
+++ DataModificer/SpeedRace/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Car> cars = new List<Car>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split();
+                string model = data[0];
+                double fuelAmouth = double.Parse(data[1]);
+                double fuelAmouthPerKm = double.Parse(data[2]);
+
+
+                Car car = new Car(model, fuelAmouth, fuelAmouthPerKm);
+
+                cars.Add(car);
+            }
+
+            string comand = Console.ReadLine();
+
+            while (comand != "End")
+            {
+                string[] data = comand.Split();
+                string model = data[1];
+                double distance = double.Parse(data[2]);
+
+                //var car = FindCar(model, cars);
+
+                Car car = cars.FirstOrDefault(x => x.Model == model);
+
+                car.Distance(car.FuelAmount, car.FuelConsumptionPerKilometer, distance);
+
+                comand = Console.ReadLine();
+            }
+
+            foreach (var item in cars)
+            {
+                Console.WriteLine($"{item.Model} {item.FuelAmount:F2} {item.TravelledDistance}");
+            }
+
+
+            
+        }
+
+        //private static Car FindCar(string model, List<Car> cars)
+        //{
+        //    Car car = null;
+        //    foreach (var item in cars)
+        //    {
+        //        if (item.Model == model)
+        //        {
+        //            car = item;
+
+        //        }
+
+        //    }
+        //    return car;
+        //}
+
+
+
+    }
+}
