e67bc80753ecf564f95e8ac1fe06b48cc2038a82
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-30 07:46:05 +0200

    !CLass

---
+++ DemoClasses/DemoClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DemoClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("CleverMoney","Stoyne", 50);
+
+            int result = book.PagesX2(111);
+
+            book.Print(book.Name, book.Autor,result);
+        }
+    }
+}
