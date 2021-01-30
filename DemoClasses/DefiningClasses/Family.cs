52485150ec3162925335eb2749da013b247d93bb
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-30 07:45:51 +0200

    DefiningClasses

---
+++ DemoClasses/DefiningClasses/Family.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+
+        public List<Person> People { get; set; }
+
+
+        public void AddPeople(Person member)
+        {
+            People.Add(member);
+        }
+
+        int maxYear = int.MinValue;
+        Person person = null;
+        public Person GetOldestMember()
+        {
+
+            foreach (var currMember in People)
+            {
+                var currAge = currMember.Age;
+
+                if(maxYear < currAge)
+                {
+                    maxYear = currAge;
+                    person = currMember;
+                }
+            }
+
+            return person;
+        }
+    }
+}
---
+++ DemoClasses/DefiningClasses/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+
+                Person person = new Person(name, age);
+
+                family.AddPeople(person);
+            }
+            
+
+           Person oldestPerson = family.GetOldestMember();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
