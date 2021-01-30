b72716c8425733e1aa0ee92001246e4df0250fd8
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-30 07:44:35 +0200

    I solved dataModifier from classes

---
+++ DataModificer/DataModificer/DateModifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModificer
+{
+    public class DateModifier
+    {
+
+        public int DataBetween(string first,string second)
+        {
+            DateTime start = DateTime.Parse(first);
+            DateTime end = DateTime.Parse(second);
+
+            int totalDays = (int)(start - end).TotalDays;
+            
+            return Math.Abs(totalDays);
+        }
+    }
+}
