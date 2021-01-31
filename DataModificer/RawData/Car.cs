ecaf7e3f4db508090e14db1cf333cf739b199365
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-01-31 09:31:31 +0200

    I solved RawData

---
+++ DataModificer/RawData/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Car
+    {
+        
+
+        public Car(Tires[] tires, Cargo cargo, Engine engine,string model)
+        {
+            this.Tires = tires;
+            this.Cargo = cargo;
+            this.Engine = engine;
+            this.Model = model;
+
+        }
+
+        public Tires[] Tires { get; set; }
+
+        public Cargo Cargo { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public string Model { get; set; }
+        public string Where { get; internal set; }
+    }
+}
---
+++ DataModificer/RawData/Engine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Engine
+    {
+        public Engine(int engineSpeed, double enginePower)
+        {
+            EngineSpeed = engineSpeed;
+            EnginePower = enginePower;
+        }
+
+        public int EngineSpeed { get; set; }
+
+        public double EnginePower { get; set; }
+
+        
+    }
+}
