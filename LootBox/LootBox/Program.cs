bf94c64bad5b15c36e156b4af34301688a795805
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-02-05 19:17:42 +0200

    I solved LoodBox

---
+++ LootBox/LootBox/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LootBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> firstField = new Queue<int>();
+            Stack<int> secondField = new Stack<int>();
+
+            
+
+            int[] numFirst = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numSeconds = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numFirst.Length; i++)
+            {
+                firstField.Enqueue(numFirst[i]);
+            }
+
+            for (int i = 0; i < numSeconds.Length; i++)
+            {
+                secondField.Push(numSeconds[i]);
+            }
+
+            int totalSum = 0;
+            int evenOrOdd = 0;
+
+            while (firstField.Count != 0 && secondField.Count != 0)
+            {
+                int a = firstField.Peek();
+                int b = secondField.Peek();
+
+                evenOrOdd = a + b;
+
+                if (evenOrOdd % 2 == 0)
+                {
+                    totalSum += firstField.Dequeue() + secondField.Pop();
+                }
+                else
+                {
+                    firstField.Enqueue(secondField.Pop());
+                }
+                
+
+            }
+
+            if(firstField.Count == 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if(totalSum >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {totalSum}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {totalSum}");
+            }
+        }
+    }
+}
