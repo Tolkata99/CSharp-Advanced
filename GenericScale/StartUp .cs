91ac3c159c04b7358baa155e346a97ad5f79b402
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-02-06 13:54:29 +0200

    I solved GenericScale

---
+++ GenericScale/StartUp .cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //int a = 4;
+            //int b = 4;
+
+            string a = "anatoli";
+            string b = "anatoli";
+            EqualityScale<string> numbers = new EqualityScale<string>(a,b);
+
+            Console.WriteLine(numbers.AreEqual());
+
+        }
+    }
+}
